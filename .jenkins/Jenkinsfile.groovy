470c9a4a7fb2825277d44b700afd9e10f5777083
lcompare <lcompare@gmail.com>
2022-11-10 22:58:11 -0300

    a

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-String[] arr = [ "one","two","three",'four','five' ]
+changes = []
 
 pipeline{
     agent { any{} }
@@ -13,17 +13,36 @@ pipeline{
         stage('Setup environment'){
             steps{
                 script{
-                    echo "nothing"
+                    changes.add('####1')
+                    changes.add('####2')
+                    result = getCRs('log.log').trim()
+                    
+                    if(result == '1'){
+                        echo "2"
+                    }
                 }
             }
         }
         stage('build') {
             steps{
                 script{
-                    echo "nothing 2"
+                    changes.each{
+                        //echo "${it}"
+                        echo "hola como andas ${it}"
+                    }
                 }
             }
         }
         
     }
+}
+
+def getCRs(logName){
+    sh label: 'Checking something', script: 'echo "this is a bash script."'
+    result = sh(returnStdout: true, script: 'echo "CHG000128271 CHG11223344"').trim()
+    str = []
+    str = result.split(" ")
+    changes.add(str[0].trim())
+    changes.add(str[1].trim())
+    return result
 }
